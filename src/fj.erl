7781f0ea2fe9c7927609d78eadc3916f4e25545a
Bryan Fink <bryan@basho.com>
2013-07-16 09:15:04 -0400

    faster than mochijson2, but also not as picky

---
+++ src/fj.erl
@@ -0,0 +1,105 @@
+%% @doc fast json parsing
+-module(fj).
+
+-export([parse/1]).
+
+-define(START_OBJECT, start_object).
+-define(START_ARRAY, start_array).
+
+%% @doc mochijson2 compatibility
+decode(Bin) ->
+    {ok, Value} = parse(Bin),
+    Value.
+
+parse(Bin) when is_binary(Bin) ->
+    case catch value(Bin, []) of
+        {error, Rest} ->
+            {error, byte_size(Bin)-byte_size(Rest)};
+        [Value] ->
+            {ok, Value}
+    end.
+
+value(<<${, Bin/binary>>, Stack) ->
+    value(Bin, [?START_OBJECT|Stack]);
+value(<<$}, Bin/binary>>, Stack) ->
+    value(Bin, end_object(Stack));
+value(<<$:, Bin/binary>>, Stack) ->
+    value(Bin, Stack);
+value(<<$[, Bin/binary>>, Stack) ->
+    value(Bin, [?START_ARRAY|Stack]);
+value(<<$], Bin/binary>>, Stack) ->
+    value(Bin, end_array(Stack));
+value(<<$,, Bin/binary>>, Stack) ->
+    value(Bin, Stack);
+value(<<$", Bin/binary>>, Stack) ->
+    str(Bin, Stack);
+value(<<"true", Bin/binary>>, Stack) ->
+    value(Bin, [true|Stack]);
+value(<<"false", Bin/binary>>, Stack) ->
+    value(Bin, [false|Stack]);
+value(<<"null", Bin/binary>>, Stack) ->
+    value(Bin, [null|Stack]);
+value(<<32, Bin/binary>>, Stack) ->    %space
+    value(Bin, Stack);
+value(<<9, Bin/binary>>, Stack) ->     %tab
+    value(Bin, Stack);
+value(<<10, Bin/binary>>, Stack) ->    %carriage return
+    value(Bin, Stack);
+value(<<13, Bin/binary>>, Stack) ->    %line feed
+    value(Bin, Stack);
+value(<<>>, Result) ->
+    Result;
+value(Bin, Stack) ->
+    num(Bin, Stack).
+
+end_array(Stack) ->
+    end_array(Stack, []).
+end_array([?START_ARRAY|Stack], Array) ->
+    [Array|Stack];
+end_array([Item|Stack], Array) ->
+    end_array(Stack, [Item|Array]).
+
+end_object(Stack) ->
+    end_object(Stack, []).
+end_object([?START_OBJECT|Stack], Obj) ->
+    [{struct, Obj}|Stack];
+end_object([Value,Key|Stack], Obj) ->
+    end_object(Stack, [{Key, Value}|Obj]).
+
+str(Bin, Stack) ->
+    str(Bin, Stack, false, []).
+str(<<$", Bin/binary>>, Stack, false, Rev) ->
+    value(Bin, [list_to_binary(lists:reverse(Rev))|Stack]);
+str(<<$\\, Bin/binary>>, Stack, false, Rev) ->
+    str(Bin, Stack, true, Rev);
+str(<<C, Bin/binary>>, Stack, _, Rev) ->
+    str(Bin, Stack, false, [C|Rev]).
+%% TODO: unicode
+
+num(Bin, Stack) ->
+    num(Bin, Stack, false, []).
+
+num(<<$-, Bin/binary>>, Stack, Dec, Rev) ->
+    num(Bin, Stack, Dec, [$-|Rev]);
+num(<<$., Bin/binary>>, Stack, _, Rev) ->
+    num(Bin, Stack, true, [$.|Rev]);
+num(<<$e, Bin/binary>>, Stack, Dec, Rev) ->
+    num(Bin, Stack, Dec, [$e|Rev]);
+num(<<$E, Bin/binary>>, Stack, Dec, Rev) ->
+    num(Bin, Stack, Dec, [$e|Rev]);
+num(<<C, Bin/binary>>, Stack, Dec, Rev) when C >= $0, C =< $9 ->
+    num(Bin, Stack, Dec, [C|Rev]);
+num(Bin, Stack, true, Rev) ->
+    case catch list_to_float(lists:reverse(Rev)) of
+        {'EXIT', _} ->
+            throw({error, Bin});
+        Float ->
+            value(Bin, [Float|Stack])
+    end;
+num(Bin, Stack, false, Rev) ->
+    case catch list_to_integer(lists:reverse(Rev)) of
+        {'EXIT', _} ->
+            throw({error, Bin});
+        Int ->
+            value(Bin, [Int|Stack])
+    end.
